b57965aa0a45699097add7acec6fa5021c1b73df
Laurentius Kevin Hendrawanto <45349515+LaurentiusKevin@users.noreply.github.com>
2019-12-23 16:27:56 +0700

    Update login.blade.php

--- resources/views/dashboard/login.blade.php
+++ resources/views/dashboard/login.blade.php
@@ -17,10 +17,10 @@
                 {{--                <div class="col-12 col-sm-8 offset-sm-2 col-md-6 offset-md-3 col-lg-6 offset-lg-3 col-xl-4 offset-xl-4">--}}
                 <div class="col-12 col-sm-8 col-md-6 col-lg-6 col-xl-4">
                     <div class="login-brand">
-                        <img src="{{ asset('assets/logo/with-name/logo-1000.png') }}" alt="logo" style="width: 40%">
+                        <img id="logo" src="{{ asset('assets/logo/with-name/logo-1000.png') }}" alt="logo" style="width: 100%">
                     </div>
 
-                    <div class="card card-danger">
+                    <div id="loginCard" class="card card-danger d-none">
                         <div class="card-header"><h4>Login</h4></div>
 
                         <div class="card-body">
@@ -91,7 +91,12 @@
     let btnSubmit = $('#btnSubmit');
     let loading = $('#loading');
 
+    let loginCard = $('#loginCard');
+    let logo = $('#logo');
+
     $(document).ready(function () {
+        logo.css('width','40%');
+        loginCard.removeClass('d-none');
         formLogin.submit(function (e) {
             btnSubmit.addClass('d-none');
             loading.removeClass('d-none');
