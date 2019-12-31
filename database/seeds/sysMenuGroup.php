95ef5b88351c81e82e1a556c593bcc33b76364a3
Laurentius Kevin Hendrawanto <45349515+LaurentiusKevin@users.noreply.github.com>
2019-12-31 11:28:41 +0700

    Update Migration Menu dan Menu Group

--- database/seeds/sysMenuGroup.php
+++ database/seeds/sysMenuGroup.php
@@ -14,24 +14,35 @@ class sysMenuGroup extends Seeder
     {
         $data = [
             [
+                'id' => '1',
                 'name' => 'System Utility',
                 'segment_name' => 'system',
                 'icon' => 'fas fa-cogs',
                 'ord' => '1'
             ],
             [
+                'id' => '2',
                 'name' => 'Master Data',
                 'segment_name' => 'master',
                 'icon' => 'fas fa-database',
                 'ord' => '2'
             ],
             [
+                'id' => '3',
                 'name' => 'Penjualan',
                 'segment_name' => 'penjualan',
                 'icon' => 'fas fa-bus-alt',
                 'ord' => '3'
             ],
             [
+                'id' => '4',
+                'name' => 'Laporan',
+                'segment_name' => 'laporan',
+                'icon' => 'fas fa-chart-line',
+                'ord' => '4'
+            ],
+            [
+                'id' => '5',
                 'name' => 'Problem',
                 'segment_name' => 'problem',
                 'icon' => 'fas fa-exclamation-triangle',
