c651b503d5c23f34268d5e6abfcc52bd707fd742
Laurentius Kevin Hendrawanto <45349515+LaurentiusKevin@users.noreply.github.com>
2020-01-02 15:09:45 +0700

    Update Summary Overview

--- app/Http/Controllers/c_Overview.php
+++ app/Http/Controllers/c_Overview.php
@@ -3,10 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class c_Overview extends Controller
 {
+    public function dataset() {
+        try {
+            $tgl = date('Y-m-d');
+//            $tgl = '2019-12-27';
+            return DB::table('transaksi')
+                ->select(
+                    'ms_penumpang.jenis',
+                    DB::raw('COUNT(transaksi.id) as total')
+                )
+                ->leftJoin('ms_penumpang','transaksi.id_penumpang','=','ms_penumpang.id')
+                ->where('tgl_transaksi','=',$tgl)
+                ->groupBy('ms_penumpang.jenis')
+                ->get();
+        } catch (\Exception $ex) {
+            return response()->json([$ex]);
+        }
+    }
+
     public function index() {
-        return view('dashboard.overview.index');
+        $data = $this->dataset();
+        return view('dashboard.overview.index')->with('transaksi',$data);
     }
 }
--- resources/views/dashboard/overview/index.blade.php
+++ resources/views/dashboard/overview/index.blade.php
@@ -2,62 +2,32 @@
 
 @section('content')
     <div class="row">
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-primary">
-                    <i class="fas fa-file-alt"></i>
+        <div class="col-lg-12 col-md-12 col-sm-12 col-12">
+            <div class="card card-statistic-2">
+                <div class="card-stats">
+                    <div class="card-stats-title">
+                        Tiket Terjual Hari Ini
+                    </div>
+                    <div class="card-stats-items">
+                        @php($total = 0)
+                        @foreach($transaksi as $t)
+                            <div class="card-stats-item">
+                                <div class="card-stats-item-count">{{ $t->total }}</div>
+                                <div class="card-stats-item-label">{{ $t->jenis }}</div>
+                            </div>
+                            @php($total += $t->total)
+                        @endforeach
+                    </div>
+                </div>
+                <div class="card-icon shadow-primary bg-primary">
+                    <i class="fas fa-ticket-alt"></i>
                 </div>
                 <div class="card-wrap">
                     <div class="card-header">
-                        <h4>Total PO Bulan Ini</h4>
+                        <h4>Total Tiket</h4>
                     </div>
                     <div class="card-body">
-                        0
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-danger">
-                    <i class="fas fa-clipboard-list"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Total Transaksi</h4>
-                    </div>
-                    <div class="card-body">
-                        0
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-warning">
-                    <i class="far fa-file"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Reports</h4>
-                    </div>
-                    <div class="card-body">
-                        0
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-lg-3 col-md-6 col-sm-6 col-12">
-            <div class="card card-statistic-1">
-                <div class="card-icon bg-success">
-                    <i class="fas fa-circle"></i>
-                </div>
-                <div class="card-wrap">
-                    <div class="card-header">
-                        <h4>Belum Notice</h4>
-                    </div>
-                    <div class="card-body">
-                        0
+                        {{ $total }}
                     </div>
                 </div>
             </div>
